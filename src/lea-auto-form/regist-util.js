1c80674c7f7c533b5b8bb384c491b0cd75568241
leaco <leaco.li@learnable.ai>
2024-07-18 16:25:32 +0800

    fix

--- src/lea-auto-form/regist-util.js
+++ src/lea-auto-form/regist-util.js
@@ -1,81 +1,93 @@
-import { inject, onMounted, onUnmounted, provide } from 'vue'
+import {inject, onMounted, onUnmounted, provide} from 'vue'
 
-export function provideRegister (childLeaAutoForms) {
-  provide('registerLeaAutoForm', registerChild)
-
-  function registerChild (component, regist) {
-    if (regist) {
-      childLeaAutoForms.value.push(component)
-      console.log(`registerLeaAutoFormSuccess, total:${childLeaAutoForms.value.length}`, component)
-    } else {
-      for (let i = 0; i < childLeaAutoForms.value.length; i++) {
-        if (childLeaAutoForms.value[i].uid === component.uid) {
-          childLeaAutoForms.value.splice(i, 1)
-          console.log('unRegisterLeaAutoFormSuccess', component.uid)
-          break
-        }
-      }
+export function provideRegister(childLeaAutoForms) {
+    try {
+        provide('registerLeaAutoForm', registerChild)
+    } catch (e) {
+        console.debug(e)
     }
-  }
-}
 
-export function registerInstance (instance) {
-  if (!instance) {
-    console.warn('registerInstance: instance is null, this instance auto validation may not work')
-    return
-  }
-  const registMeFun = inject('registerLeaAutoForm')
-  if (registMeFun) {
-    onMounted(() => {
-      if (typeof registMeFun === 'function') {
-        registMeFun(instance, true)
-      }
-    })
-    onUnmounted(() => {
-      if (typeof registMeFun === 'function') {
-        registMeFun(instance, false)
-      }
-    })
-  }
-}
-
-export function providePublicRegister (childLeaAutoForms) {
-  provide('publicRegisterLeaAutoForm', registerChild)
-
-  function registerChild (component, regist) {
-    if (regist) {
-      childLeaAutoForms.value.push(component)
-      console.log(`registerLeaAutoFormSuccess total:${childLeaAutoForms.value.length}`, component)
-    } else {
-      for (let i = 0; i < childLeaAutoForms.value.length; i++) {
-        if (childLeaAutoForms.value[i].uid === component.uid) {
-          childLeaAutoForms.value.splice(i, 1)
-          console.log('publicUnRegisterLeaAutoFormSuccess', component.uid)
-          break
+    function registerChild(component, regist) {
+        if (regist) {
+            childLeaAutoForms.value.push(component)
+            console.debug(`registerLeaAutoFormSuccess, total:${childLeaAutoForms.value.length}`, component)
+        } else {
+            for (let i = 0; i < childLeaAutoForms.value.length; i++) {
+                if (childLeaAutoForms.value[i].uid === component.uid) {
+                    childLeaAutoForms.value.splice(i, 1)
+                    console.debug('unRegisterLeaAutoFormSuccess', component.uid)
+                    break
+                }
+            }
         }
-      }
     }
-  }
 }
 
-export function publicRegisterInstance (instance) {
-  if (!instance) {
-    console.warn('publicRegisterInstance: instance is null, this instance auto validation may not work')
-    return
-  }
-  const registMeFun = inject('publicRegisterLeaAutoForm')
-  if (registMeFun) {
-    onMounted(() => {
-      if (typeof registMeFun === 'function') {
-        registMeFun(instance, true)
-      }
-    })
-    onUnmounted(() => {
-      if (typeof registMeFun === 'function') {
-        registMeFun(instance, false)
-      }
-    })
-  } else {
-    console.warn('can not found publicRegisterLeaAutoForm, this form auto validation may not work', instance)
-  }
+export function registerInstance(instance) {
+    if (!instance) {
+        console.warn('registerInstance: instance is null, this instance auto validation may not work')
+        return
+    }
+    try {
+        const registMeFun = inject('registerLeaAutoForm')
+        if (registMeFun) {
+            onMounted(() => {
+                if (typeof registMeFun === 'function') {
+                    registMeFun(instance, true)
+                }
+            })
+            onUnmounted(() => {
+                if (typeof registMeFun === 'function') {
+                    registMeFun(instance, false)
+                }
+            })
+        }
+    } catch (e) {
+        console.debug(e)
+    }
+}
+
+export function providePublicRegister(childLeaAutoForms) {
+    provide('publicRegisterLeaAutoForm', registerChild)
+
+    function registerChild(component, regist) {
+        if (regist) {
+            childLeaAutoForms.value.push(component)
+            console.log(`registerLeaAutoFormSuccess total:${childLeaAutoForms.value.length}`, component)
+        } else {
+            for (let i = 0; i < childLeaAutoForms.value.length; i++) {
+                if (childLeaAutoForms.value[i].uid === component.uid) {
+                    childLeaAutoForms.value.splice(i, 1)
+                    console.log('publicUnRegisterLeaAutoFormSuccess', component.uid)
+                    break
+                }
+            }
+        }
+    }
+}
+
+export function publicRegisterInstance(instance) {
+    if (!instance) {
+        console.warn('publicRegisterInstance: instance is null, this instance auto validation may not work')
+        return
+    }
+    try {
+        const registMeFun = inject('publicRegisterLeaAutoForm')
+        if (registMeFun) {
+            onMounted(() => {
+                if (typeof registMeFun === 'function') {
+                    registMeFun(instance, true)
+                }
+            })
+            onUnmounted(() => {
+                if (typeof registMeFun === 'function') {
+                    registMeFun(instance, false)
+                }
+            })
+        } else {
+            console.debug('can not found publicRegisterLeaAutoForm, this form auto validation may not work', instance)
+        }
+    } catch (e) {
+        console.debug(e)
+    }
 }
